1a323af5818ec58a8813525fb6f2bf85472064da
AtlasVIA <atlas@vialabs.io>
2024-10-31 10:40:03 +0100

    added fleek deployments

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -1,17 +1,13 @@
-// @ts-check
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
-  eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
-  },
+  trailingSlash: true,
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
 };
